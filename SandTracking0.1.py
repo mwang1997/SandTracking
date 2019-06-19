929030dee69a61e17b60cef5c05554c00d1f69bb
Mark Wang <markwang@v129-22-124-220.VCLIENT.CWRU.Edu>
2019-06-19 14:36:28 +0800

    altered for scaling

--- SandTracking0.1.py
+++ SandTracking0.1.py
@@ -3,16 +3,25 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import av
+import trackpy as tp
+
 import pandas as pd
 from pandas import DataFrame, Series
 
 import pims
-import av
-import trackpy as tp
+from pims import pipeline
+
+#Transforms list of frames in numpy array format into greyscale
+def to_greyscale(frame):
+	red = frame[:, :, 0]
+	green = frame[:, :, 1]
+	blue = frame[:, :, 2]
+	return 0.2126 * red + 0.7152 * green + 0.0722 * blue
 
 print("Enter name of video")
 
-videoName = input()
+videoName = "/Users/markwang/Documents/LZU/Recordings/" + input()
 
 print("Enter average size of particles in pixels, as an odd number")
 
@@ -23,11 +32,15 @@ print("Enter the parameter mass to filter detection")
 particleTolerance = int(input())
 
 #Frames is an numpy array of video frames
-frames = pims.Video("/Users/markwang/Documents/LZU/Recordings/" + videoName).to_ndArray()
+frames = []
+container = av.open(videoName)
+for frame in container.decode(video = 0):
+	frames.append(frame.to_ndarray())
 
-#F is a list of data for each Frame of Frames
+#f is a list of data for each Frame of Frames
 f = []
 
 for frame in frames:
-	f.append(tp.locate(frame, particalSize, invert = False, minmass = particalTolerance))
+	f.append(tp.locate(frame, particleSize, invert = False, minmass = particleTolerance))
+
 
