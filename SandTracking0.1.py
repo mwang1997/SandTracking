7e6d9e0081ba56dc0e22b62f5867e277067f3c69
Mark Wang <markwang@v129-22-124-11.VCLIENT.CWRU.Edu>
2019-06-21 01:31:50 +0800

    Added multiframe tracking

--- SandTracking0.1.py
+++ SandTracking0.1.py
@@ -3,21 +3,14 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-import av
 import trackpy as tp
+import av
 
 import pandas as pd
 from pandas import DataFrame, Series
 
 import pims
 from pims import pipeline
-
-#Transforms list of frames in numpy array format into greyscale
-def to_greyscale(frame):
-	red = frame[:, :, 0]
-	green = frame[:, :, 1]
-	blue = frame[:, :, 2]
-	return 0.2126 * red + 0.7152 * green + 0.0722 * blue
 
 print("Enter name of video")
 
@@ -27,23 +20,24 @@ print("Enter average size of particles in pixels, as an odd number")
 
 particleSize = int(input())
 
-print("Enter the parameter mass to filter detection")
+print("Enter the parameter minimum illumination mass to filter detection")
 
 particleTolerance = int(input())
 
-#Frames is an numpy array of video frames
-frames = []
-container = av.open(videoName)
-for frame in container.decode(video = 0):
-	frames.append(frame.to_ndarray())
+print("Enter the frame to start from")
 
-plt.imshow(frames[0])
+startFrame = int(input())
+
+#frames is an numpy array of video frames
+frames = pims.as_grey(pims.PyAVReaderIndexed(videoName))
+
+#f is the DataFrame of VideoFrames
+f = tp.batch(frames[startFrame: startFrame + 50], particleSize, invert = False, minmass = particleTolerance)
+
+tp.annotate(f, frames[1000])
+fig, ax = plt.subplots()
+ax.hist(f['mass'], bins=1000)
 plt.show()
-
-#f is a list of data for each Frame of Frames
-f = []
-
-for frame in frames:
-	f.append(tp.locate(frame, particleSize, invert = False, minmass = particleTolerance))
+ax.show()
 
 
